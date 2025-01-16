feab470ab9a5b80c47700ef4e574eb32565bc479
Ali05g <allinsonsance@gmail.com>
2025-01-15 20:34:27 -0600

    GataBotLite-MD~Gata Dios

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ import { en, es, id, ar, pt, de, it } from './lib/idiomas/total-idiomas.js'
 // ES ➜ Si desea recibir reportes debe de usar los tres parámetros (Número, nombre y true)
 // EN ➜ Add the Bot number as priority first
 // EN ➜ If you want to receive reports you must use the three parameters (Number, name and true)
-global.owner = [
+global.owner = [ 🩷Diosa🩷]
 ['5214774444444', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 🌻🐈', true], //𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 🌻🐈
 ['593968263524', '𝙂𝘽 𝘼𝙨𝙞𝙨𝙩𝙚𝙣𝙘𝙞𝙖 🌻🐈', true],
 ['5218715746374'],
@@ -21,7 +21,7 @@ global.owner = [
 ['593968585383'],
 ['51968382008']]
 
-global.mods = [] 
+global.mods = [7] 
 global.prems = []
 
 // Cambiar a false para desactivar en "auto-reconexion" de sub-bots
